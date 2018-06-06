d7868f48f79c294a2a29b76b990f9de14d2bac2b
anthony <729915632@qq.com>
2018-06-06 17:49:31 +0800

    首页60%

--- website/build/webpack.base.conf.js
+++ website/build/webpack.base.conf.js
@@ -13,7 +13,8 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app:  ["babel-polyfill",'./src/main.js']
+    polyfill:"babel-polyfill",
+    app:  './src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
