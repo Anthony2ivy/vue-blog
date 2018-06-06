e48e13ce064b9e33ce40c07a650f2aadaee2a985
anthony <729915632@qq.com>
2018-06-06 17:15:29 +0800

    首页60%

--- website/src/main.js
+++ website/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -8,7 +9,7 @@ import 'at-ui-style'
 import common from '@/assets/css/common.css'
 import my from '@/assets/css/my.css'
 import VueParticles from 'vue-particles'
-import 'babel-polyfill'
+
 
 Vue.config.productionTip = false;
 Vue.use(AtComponents);
