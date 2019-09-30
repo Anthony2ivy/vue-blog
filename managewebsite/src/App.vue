b902dc36f9504d64521f429ffa9e977b3b499fc8
Anthony <729915632@qq.com>
2019-09-30 14:38:31 +0800

    修复登录逻辑

--- managewebsite/src/App.vue
+++ managewebsite/src/App.vue
@@ -1,61 +1,75 @@
 <template>
-  <div id="app">
-    <header class="admin-header">
-      <div class="header-logo-text">MY BLOG</div>
-    </header>
-    <div id="main-container">
-      <router-view/>
-    </div>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
+
 <script>
+  import blogService from './services/blogService';
+
   export default {
     name: 'App',
-    data: function () {
+    data: function() {
       return {
-        isLogin:false
-      }
+        navItems: [],
+        isTop: true,
+        mainTheme: 'dark',
+        activeItem: this.$route.name,
+        isLogin: false,
+      };
     },
     methods: {},
-    mounted: function () {
-
-    }
-  }
+    mounted: function() {
+      let self = this;
+      blogService.checkLogin().then(() => {
+        self.$root.isLogin = true;
+      }).catch(e => {
+        debugger;
+        self.$root.isLogin = false;
+        if (this.$route.path != '/login') {
+          this.$router.push( '/login');
+        }
+      });
+      window.onscroll = function() {
+        const scrollTop = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop);
+        if (scrollTop > 0) {
+          if (self.isTop)
+            self.isTop = false;
+        } else {
+          if (!self.isTop)
+            self.isTop = true;
+        }
+      };
+    },
+  };
 </script>
 
 <style>
-  :root {
-    --header-color: #2C405A;
-    --header-height: 50px;
-    --main-font:  -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
-    --brand-color: #6190E8;
-  }
-  #app{
+  body {
+    font-family: "Microsoft YaHei", 微软雅黑, "MicrosoftJhengHei", 华文细黑, STHeiti, MingLiu;
   }
 
-  body{
-    font-family: var(--main-font);
-  }
-  .admin-header {
-    background-color: var(--header-color);
-    width: 100%;
-    height: var(--header-height);
-    display: flex;
-    flex-direction: column;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  .left-menu {
+    min-height: 100vh;
     position: fixed;
   }
-  .header-logo-text{
-    width: 200px;
-    font-size: 20px;
-    line-height: 20px;
-    height: 20px;
+
+  .center-title {
+    font-weight: 500;
     color: white;
-    margin-left: 60px;
-    font-family: 'Helvetica Neue';
+    text-align: center;
+    margin-top: 30px;
+    font-size: 25px;
+    margin-bottom: 20px;
+  }
+
+  .right-container {
+    padding-left: 250px;
+    padding-top: 10px;
+    padding-right: 10px;
+    background-color: rgb(238, 238, 238);
+    height: 100vh;
   }
 
 </style>
--- managewebsite/src/router/index.js
+++ managewebsite/src/router/index.js
@@ -1,18 +1,32 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from '../pages/Login'
-Vue.use(Router)
+import Vue from 'vue';
+import Router from 'vue-router';
+import loginPage from '../pages/loginPage';
+import lefMenu from  '../pages/leftMenu'
+import homePage from  '../pages/homePage'
+import blogsPage from '../pages/blogsPage'
+
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'login',
-      redirect: 'login'
+      redirect: 'login',
     },{
-     path:'/login',
-      name:'login',
-      component:Login
+      path: '/login',
+      name: 'login',
+      component: loginPage
+    },{
+      path: '/main',
+      name: 'main',
+      component: lefMenu,
+      redirect:'mian/home'
+    },
+    {
+      path: '/main/home',
+      name: 'main.home',
+      component: homePage
     }
-  ]
-})
+  ],
+});
