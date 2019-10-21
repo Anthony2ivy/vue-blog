e10bbf453717868a07f86babaa11d49aafccf8ce
Anthony <729915632@qq.com>
2019-10-21 21:26:23 +0800

    修复登录逻辑

--- managewebsite/src/App.vue
+++ managewebsite/src/App.vue
@@ -1,30 +1,34 @@
 <template>
   <div>
-    <Menu class="left-menu" :theme="mainTheme" :active-name="activeItem">
+    <Menu class="left-menu" :theme="mainTheme" :active-name="activeItem" v-if="this.$route.name !='login'" @on-select="selectMenu" >
       <div class="center-icon" style="padding-top: 10px">
         <div  class="my-double-circle-icon" style="margin-top: 10px;padding-top: 5px; background-image: url('static/imgs/icon.jpg'); width: 100px; height: 100px;"></div>
       </div>
       <div class="center-title">MY BLOG</div>
-        <MenuItem name="home" to="home">
-          <Icon type="md-home" />
-          首页
-        </MenuItem>
+      <MenuItem name="home" to="home">
+        <Icon type="md-home" />
+        首页
+      </MenuItem>
       <MenuItem name="newBlog" to="newBlog">
         <Icon type="md-send" />
         发布新博客
       </MenuItem>
-        <MenuItem name="blogList" to="blogs">
-          <Icon type="md-list" />
-          博客管理
-        </MenuItem>
-        <MenuItem name="tag">
-          <Icon type="md-pricetag" />
-          标签管理
-        </MenuItem>
-        <MenuItem name="setting">
-          <Icon type="md-settings" />
-          设置
-        </MenuItem>
+      <MenuItem name="blogList" to="blogs">
+        <Icon type="md-list" />
+        博客管理
+      </MenuItem>
+      <MenuItem name="tag">
+        <Icon type="md-pricetag" />
+        标签管理
+      </MenuItem>
+      <MenuItem name="setting">
+        <Icon type="md-settings" />
+        设置
+      </MenuItem>
+      <MenuItem name="logout">
+        <Icon type="md-log-out" />
+        退出
+      </MenuItem>
     </Menu>
     <router-view class="right-container"> </router-view>
   </div>
@@ -32,6 +36,7 @@
 
 
 <script>
+  import blogService from './services/blogService'
   export default {
     name: 'App',
     data: function () {
@@ -43,7 +48,15 @@
         activeItem: this.$route.name
       }
     },
-    methods: {},
+    methods: {
+      selectMenu(name){
+        if(name=='logout'){
+          blogService.logout().finally(()=>{
+            this.$root.isLogin=false;
+          });
+        }
+      }
+    },
     mounted: function () {
       var self=this;
       window.onscroll = function () {
@@ -64,10 +77,10 @@
   body{
     font-family: "Microsoft YaHei", 微软雅黑, "MicrosoftJhengHei", 华文细黑, STHeiti, MingLiu;
   }
-.left-menu{
-  min-height: 100vh;
-  position: fixed;
-}
+  .left-menu{
+    min-height: 100vh;
+    position: fixed;
+  }
   .center-title{
     font-weight: 500;
     color: white;
